70a126fba18b234c24faf088d54251e5f7d3d91d
syl-craft <mahe.sylvain@gmail.com>
2026-06-30 18:39:31 +0200

    pwa: service worker

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'mediatheque-v1';
+const ASSETS = [
+  '/mediatheque-horaires/',
+  '/mediatheque-horaires/index.html',
+  '/mediatheque-horaires/manifest.json',
+  '/mediatheque-horaires/icon-192.png',
+  '/mediatheque-horaires/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
